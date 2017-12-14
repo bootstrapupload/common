1ee7cc7d49a79dc8725958cd45fd48b0ddfe3885
XuQuan <xuquanchen@XuQuandeMacBook-Pro.local>
2017-12-14 11:15:49 +0800

    初始化ios项目

---
+++ app/Hackathons/Hackathons/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Hackathons
+//
+//  Created by XuQuan on 14/12/2017.
+//  Copyright © 2017 XuQuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ app/Hackathons/HackathonsTests/HackathonsTests.m
@@ -0,0 +1,39 @@
+//
+//  HackathonsTests.m
+//  HackathonsTests
+//
+//  Created by XuQuan on 14/12/2017.
+//  Copyright © 2017 XuQuan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HackathonsTests : XCTestCase
+
+@end
+
+@implementation HackathonsTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
